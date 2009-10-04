53ca2334bd4dcf03b562edf98af57082e385c0da
Dmitry Chirskov <freemindman@yandex.ru>
2009-10-04 19:15:07 +0600

    Fix wrong indentation.

--- obkey_classes.py
+++ obkey_classes.py
@@ -966,41 +966,41 @@ def xml_find_node(elt, name):
 		return None
 
 def fixed_writexml(self, writer, indent="", addindent="", newl=""):
-    # indent = current indentation
-    # addindent = indentation to add to higher levels
-    # newl = newline string
-    writer.write(indent+"<" + self.tagName)
-    	
-    attrs = self._get_attributes()
-    a_names = attrs.keys()
-    a_names.sort()
-    
-    for a_name in a_names:
-        writer.write(" %s=\"" % a_name)
-        xml.dom.minidom._write_data(writer, attrs[a_name].value)
-        writer.write("\"")
-    if self.childNodes:
-        if len(self.childNodes) == 1 \
-          and self.childNodes[0].nodeType == xml.dom.minidom.Node.TEXT_NODE:
-            writer.write(">")
-            self.childNodes[0].writexml(writer, "", "", "")
-            writer.write("</%s>%s" % (self.tagName, newl))
-            return
-        writer.write(">%s" % newl)
-        for node in self.childNodes:
-            fixed_writexml(node, writer,indent+addindent,addindent,newl)
-        writer.write("%s</%s>%s" % (indent,self.tagName,newl))
-    else:
-        writer.write("/>%s"%(newl))
+	# indent = current indentation
+	# addindent = indentation to add to higher levels
+	# newl = newline string
+	writer.write(indent+"<" + self.tagName)
+	
+	attrs = self._get_attributes()
+	a_names = attrs.keys()
+	a_names.sort()
+	
+	for a_name in a_names:
+		writer.write(" %s=\"" % a_name)
+		xml.dom.minidom._write_data(writer, attrs[a_name].value)
+		writer.write("\"")
+	if self.childNodes:
+		if len(self.childNodes) == 1 \
+		and self.childNodes[0].nodeType == xml.dom.minidom.Node.TEXT_NODE:
+			writer.write(">")
+			self.childNodes[0].writexml(writer, "", "", "")
+			writer.write("</%s>%s" % (self.tagName, newl))
+			return
+		writer.write(">%s" % newl)
+		for node in self.childNodes:
+			fixed_writexml(node, writer,indent+addindent,addindent,newl)
+		writer.write("%s</%s>%s" % (indent,self.tagName,newl))
+	else:
+		writer.write("/>%s"%(newl))
 
 def fixed_toprettyxml(self, indent="", addindent="\t", newl="\n"):
-    # indent = current indentation
-    # addindent = indentation to add to higher levels
-    # newl = newline string
-    writer = StringIO()
-
-    fixed_writexml(self, writer, indent, addindent, newl)
-    return writer.getvalue()
+	# indent = current indentation
+	# addindent = indentation to add to higher levels
+	# newl = newline string
+	writer = StringIO()
+	
+	fixed_writexml(self, writer, indent, addindent, newl)
+	return writer.getvalue()
 
 #=====================================================================================
 # Openbox Glue
